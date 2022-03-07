22fbbfff4aa57aaae8272a7546f57cc64437ff16
brandonkennethadkins <101079017+brandonkennethadkins@users.noreply.github.com>
2022-03-07 17:38:15 -0600

    Cs files

---
+++ Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.BankApp
+{
+   public class Account
+    {
+        private string owner;
+        private int id;
+        private decimal balance;
+        private string type;
+
+
+        public Account(string owner, int id,decimal balance, string type)
+        {
+            this.owner= owner;
+            this.id = id;
+            this.balance= balance;
+            //could have created inherited classes for each account type, but this was a faster method. 
+            this.type= type;
+            
+        }
+
+        public void Deposit(decimal amount)
+        {
+           this.balance += amount;
+     
+        }
+
+        public virtual void Withdrawal(decimal amount, bool isTransferII=false)
+        {
+            if (type == "Investment-Individual" && amount >500.0m &&isTransferII==false)
+            {
+                //do nothing
+            }
+            else
+            {
+                balance -= amount;
+            }
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public int ID
+        {
+            get { return id; }
+        }
+
+
+        public string Type
+        {
+            get { return type; }
+        }
+
+        public String Owner
+        {
+            get { return owner; }
+        }
+    }
+
+
+
+}
---
+++ Bank.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.BankApp
+{
+    class Bank
+    {
+       public String name;
+       public int bankID;
+       public List<Account> accounts;
+
+        public Bank(string name, int bankID)
+        {
+            this.name = name;
+            this.bankID = bankID;
+            this.accounts = new List<Account>();
+
+        }
+
+        public void Deposit(decimal amount, int accountID)
+        {
+            if (amount > 0)
+            {
+                GetAccountById(accountID).Deposit(amount);
+            }
+        }
+
+        public void Withdrawal(decimal amount, int accountID,bool isTransfer=false)
+        {
+            if (amount > 0)
+            {
+                GetAccountById(accountID).Withdrawal(amount);
+            }
+        }
+
+
+        public void Transfer(decimal amount, int transferFromID, int transferToID)
+        {
+            if (amount > 0)
+            {
+                Account toWithdraw = GetAccountById(transferFromID);
+                Account ToDeposit = GetAccountById(transferToID);
+                if (toWithdraw.Owner.Equals(ToDeposit.Owner))
+                {
+                    toWithdraw.Withdrawal(amount, true);
+
+                    ToDeposit.Deposit(amount);
+                }
+                else
+                {
+                    //throw new Exception("Owner Id's do not match");
+                }
+            }
+        }
+
+        public void AddAccount(string owner, int ownerID, decimal balance, string type)
+        {
+            accounts.Add(new Account(owner, ownerID, balance, type));
+        }
+
+
+
+        public String Name
+        {
+            get { return name; }
+        }
+
+        public List<Account> Accounts
+        {
+            get { return accounts; }
+        }
+
+        public Account GetAccountById(int id)
+        {
+            Account temp = accounts.FirstOrDefault(account => account.ID == id);
+            return temp;
+        }
+
+        public void AddAccount(Account newAccount)
+        {
+            accounts.Add(newAccount);
+        }
+    }
+}
